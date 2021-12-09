5853fe78189d1a7c4f3d8d4ca0c13d9fe3fddc2c
Edwin Xu <edwinhuish@gmail.com>
2021-12-10 01:07:32 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ email }}</h1>
+    <h2>{{ isValidEmail(email) }}</h2>
+    <hello-world />
+  </div>
+</template>
+
+<script>
+
+import * as Validate from './utils/validate.js'
+
+import HelloWorld from './components/HelloWorld/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      email: 'aaa@example.com'
+    }
+  },
+  methods: {
+    isValidEmail(email) {
+      return Validate.validEmail(email)
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="example">{{ msg }}</span>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello world!',
+      color: 'blue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.example {
+  color: v-bind('color');
+}
+</style>
